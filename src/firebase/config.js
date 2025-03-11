cb7816f9c7363b27f480eeadb7a58fcfafdb877f
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-11 16:11:24 -0300

    Integrando Firebase ao React

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firebase";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBb81yXE_JnPVVWmWCKrMpkI9bg1ixV8Wg",
+  authDomain: "miniblog-8c673.firebaseapp.com",
+  projectId: "miniblog-8c673",
+  storageBucket: "miniblog-8c673.firebasestorage.app",
+  messagingSenderId: "545434948092",
+  appId: "1:545434948092:web:c256fb2930b9dcb380e279",
+  measurementId: "G-KTGHRVC1PJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+export{db};
